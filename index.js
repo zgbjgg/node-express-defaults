3200d9e892c38fb3483551c8ed7879a4c844afdb
zgbjgg <zgbjgg@gmail.com>
2016-06-15 12:09:41 -0500

    add support for defaults in body

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function(defaults) {
   defaults = defaults || {};
   var paramDefaults = defaults.params;
   var queryDefaults = defaults.query;
+  var bodyDefaults = defaults.body;
 
   return function(req, res, next) {
     if (paramDefaults) {
@@ -17,6 +18,14 @@ module.exports = function(defaults) {
         if (req.query[key] === undefined) {
           req.query[key] = queryDefaults[key];
         }
+      });
+    }
+
+    if (bodyDefaults) {
+      Object.keys(bodyDefaults).forEach(function(key) {
+        if (req.body[key] === undefined) {
+          req.body[key] = bodyDefaults[key];
+        }   
       });
     }
 
